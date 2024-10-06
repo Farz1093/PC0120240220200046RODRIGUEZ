f2d6c391480d9e81927b62ecba0704113c79ced6
Fernando Rodriguez <feranroze@gmail.com>
2024-10-05 20:35:16 -0500

    Adición de Insert

---
+++ PC0120240220200046RODRIGUEZ.DOMAIN/Core/Interfaces/IAttendeesRepository.cs
@@ -0,0 +1,10 @@
+﻿using PC0120240220200046RODRIGUEZ.DOMAIN.Core.Entities;
+
+namespace PC0120240220200046RODRIGUEZ.DOMAIN.Core.Interfaces
+{
+    public interface IAttendeesRepository
+    {
+        Task<IEnumerable<Attendees>> GetAttendes();
+        Task<int> Insert(Attendees attendees);
+    }
+}
--- PC0120240220200046RODRIGUEZ.DOMAIN/Infrastructure/Repositories/AttendeesRepository.cs
+++ PC0120240220200046RODRIGUEZ.DOMAIN/Infrastructure/Repositories/AttendeesRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PC0120240220200046RODRIGUEZ.DOMAIN.Core.Entities;
+using PC0120240220200046RODRIGUEZ.DOMAIN.Core.Interfaces;
 using PC0120240220200046RODRIGUEZ.DOMAIN.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace PC0120240220200046RODRIGUEZ.DOMAIN.Infrastructure.Repositories
 {
-    public class AttendeesRepository
+    public class AttendeesRepository : IAttendeesRepository
     {
         private readonly EventManagementDbContext _dbContext;
 
@@ -19,17 +20,23 @@ namespace PC0120240220200046RODRIGUEZ.DOMAIN.Infrastructure.Repositories
         }
 
         public async Task<IEnumerable<Attendees>> GetAttendes()
-        { 
-        
+        {
+
             var attendes = await _dbContext.Attendees.ToListAsync();
             return attendes;
         }
 
         public async Task<int> Insert(Attendees attendees)
-            
+        {
+            await _dbContext.Attendees.AddAsync(attendees);
+            int rows = await _dbContext.SaveChangesAsync();
+            return rows > 0 ? attendees.Id : -1;
+
+        }
 
 
-       
+
+
 
 
 
