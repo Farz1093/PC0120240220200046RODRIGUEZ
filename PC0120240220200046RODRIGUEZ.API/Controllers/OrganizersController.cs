cd5cf90a5649bc030fdae1162eed5b60f48223af
Fernando Rodriguez <feranroze@gmail.com>
2024-10-05 20:20:42 -0500

    Adicion de OrganizersController

---
+++ PC0120240220200046RODRIGUEZ.API/Controllers/OrganizersController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PC0120240220200046RODRIGUEZ.API.Controllers
+{
+    public class OrganizersController : Controller
+    {
+        // GET: OrganizerController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: OrganizerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: OrganizerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrganizerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrganizerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OrganizerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrganizerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OrganizerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
