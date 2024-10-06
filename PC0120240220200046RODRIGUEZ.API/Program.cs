0c29126f1b24126430b7b0e051710147044d6b03
Fernando Rodriguez <feranroze@gmail.com>
2024-10-05 20:59:56 -0500

    Corrección de Program cs

--- PC0120240220200046RODRIGUEZ.API/Program.cs
+++ PC0120240220200046RODRIGUEZ.API/Program.cs
@@ -1,15 +1,18 @@
 using Microsoft.EntityFrameworkCore;
+using PC0120240220200046RODRIGUEZ.DOMAIN.Core.Interfaces;
 using PC0120240220200046RODRIGUEZ.DOMAIN.Infrastructure.Data;
+using PC0120240220200046RODRIGUEZ.DOMAIN.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 // Add services to the container.
 var _config = builder.Configuration;
 var cnx = _config.GetConnectionString("DevConnection");
 builder.Services
     .AddDbContext<EventManagementDbContext>
     (options => options.UseSqlServer(cnx));
+
+builder.Services.AddTransient<IAttendeesRepository, AttendeesRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
