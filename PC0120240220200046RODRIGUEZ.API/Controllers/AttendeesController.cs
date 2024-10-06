6cf6cbf515ea3d72ab9818325252f4dfdc0190a0
Fernando Rodriguez <feranroze@gmail.com>
2024-10-05 20:48:40 -0500

    Actualizacion de Attendees controller

--- PC0120240220200046RODRIGUEZ.API/Controllers/AttendeesController.cs
+++ PC0120240220200046RODRIGUEZ.API/Controllers/AttendeesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PC0120240220200046RODRIGUEZ.DOMAIN.Core.Interfaces;
+using PC0120240220200046RODRIGUEZ.DOMAIN.Infrastructure.Repositories;
 
 namespace PC0120240220200046RODRIGUEZ.API.Controllers
 {
@@ -7,5 +9,22 @@ namespace PC0120240220200046RODRIGUEZ.API.Controllers
     [ApiController]
     public class AttendeesController : ControllerBase
     {
+        private readonly IAttendeesRepository _attendeesRepository;
+        public AttendeesController(IAttendeesRepository attendeesRepository)
+        {
+            _attendeesRepository = attendeesRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAttendes()
+        {
+            var attendees = await _attendeesRepository.GetAttendes();
+            return Ok(attendees);
+        }
+
+
+
+
+
     }
 }
