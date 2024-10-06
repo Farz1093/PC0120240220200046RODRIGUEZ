3f6c9f1cb751df82c3a9440a4e406517e30f8607
Fernando Rodriguez <feranroze@gmail.com>
2024-10-05 20:16:00 -0500

    Más adición

--- PC0120240220200046RODRIGUEZ.API/Program.cs
+++ PC0120240220200046RODRIGUEZ.API/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PC0120240220200046RODRIGUEZ.DOMAIN.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add services to the container.
+var _config = builder.Configuration;
+var cnx = _config.GetConnectionString("DevConnection");
+builder.Services
+    .AddDbContext<EventManagementDbContext>
+    (options => options.UseSqlServer(cnx));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
